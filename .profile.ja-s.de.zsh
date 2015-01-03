c7cee03e65a05be416cd30761fe2819409719a6d
Jascha Starke <jascha@ja-s.de>
2015-01-03 20:10:47 +0100

    ret_status-Fix

--- .profile.ja-s.de.zsh
+++ .profile.ja-s.de.zsh
@@ -1,2 +1,2 @@
 local ret_status="%(?::%{$fg_bold[red]%}[%?] )"
-PROMPT='%(!..%{$fg_bold[green]%}%n%{$fg_bold[red]%}@)%{$fg_bold[red]%}%m %{$fg_bold[blue]%}%~ $(ret_status)%_$(prompt_char)%{$reset_color%} '
+PROMPT='%(!..%{$fg_bold[green]%}%n%{$fg_bold[red]%}@)%{$fg_bold[red]%}%m %{$fg_bold[blue]%}%~ ${ret_status}%_$(prompt_char)%{$reset_color%} '
