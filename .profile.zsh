da1a073f2aac506af7f2474a64ab83132cda9260
Jascha Starke <jascha.git@ja-s.de>
2015-07-20 21:33:58 +0200

    ZSH ignore leading space in history

--- .profile.zsh
+++ .profile.zsh
@@ -5,6 +5,7 @@ function prompt_char {
 local ret_status="%(?::%{$fg_bold[red]%}[%?] )"
 PROMPT='%(!..%{$fg_bold[green]%}%n%{$fg_bold[cyan]%}@)%{$fg_bold[cyan]%}%m %{$fg_bold[blue]%}%~ ${ret_status}%_$(prompt_char)%{$reset_color%} '
 
+setopt HIST_IGNORE_SPACE
 #ZSH_THEME_GIT_PROMPT_PREFIX="("
 #ZSH_THEME_GIT_PROMPT_SUFFIX=") "
 
